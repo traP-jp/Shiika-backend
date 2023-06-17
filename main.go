c96c6b63ab2d041eb2840ec11b9ceca44f81e998
ramdos0207 <dev@ramdos.net>
2023-06-17 11:13:17 +0900

    dekita

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	db *sqlx.DB
+)
 
 func main() {
-	fmt.Println("なろう講習会へようこそ！")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := mysql.Config{
+		User:      os.Getenv("DB_USERNAME"),
+		Passwd:    os.Getenv("DB_PASSWORD"),
+		Net:       "tcp",
+		Addr:      os.Getenv("DB_HOSTNAME") + ":" + os.Getenv("DB_PORT"),
+		DBName:    os.Getenv("DB_DATABASE"),
+		ParseTime: true,
+		Collation: "utf8mb4_unicode_ci",
+		Loc:       jst,
+	}
+
+	_db, err := sqlx.Open("mysql", conf.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("conntected")
+	db = _db
+
+	e := echo.New()
+
+	e.GET("/user", getUserHandler)
+
+	e.Start(":8080")
+}
+
+func getUserHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
+	var test User
+	if err := db.Get(&test, "SELECT * FROM user WHERE name='ramdos'"); errors.Is(err, sql.ErrNoRows) {
+		return echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("No such city Name = %s", cityName))
+	} else if err != nil {
+		log.Fatalf("DB Error: %s", err)
+	}
+
+	return c.JSON(http.StatusOK, test.Password)
 }
--- model.go
+++ model.go
@@ -1,17 +1,17 @@
 package main
 
 type User struct {
-	Id       int    `json:"id,omitempty" db:"id"`
+	Id       string `json:"id,omitempty" db:"id"`
 	Name     string `json:"name,omitempty" db:"name"`
 	Password string `json:"password,omitempty"  db:"password"`
 }
 type Kaminoku struct {
-	Id      int    `json:"id,omitempty" db:"id"`
+	Id      string `json:"id,omitempty" db:"id"`
 	Content string `json:"name,omitempty" db:"content"`
 	Userid  int    `json:"userid,omitempty" db:"userid"`
 }
 type Simonoku struct {
-	Id         int    `json:"id,omitempty" db:"id"`
+	Id         string `json:"id,omitempty" db:"id"`
 	Content    string `json:"name,omitempty" db:"content"`
 	KaminokuId int    `json:"kaminokuid,omitempty" db:"kaminokuid"`
 	Userid     int    `json:"userid,omitempty" db:"userid"`
